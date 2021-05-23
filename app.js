fda2190b86f2501fc7273028df9714a02ffe44ff
bhanu <bhanu.sharma456@gmail.com>
2021-05-23 13:12:05 +0530

    Finished authors route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
 const bookRouter = require('./routes/books');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const app = express();
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true, useUnifiedTopology: true})
@@ -24,7 +25,7 @@ app.set('layout','layouts/layout')
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({limit: '10mb',extended: false}));
-
+app.use(methodOverride('_method'));
 
 app.use('/',indexRouter);
 app.use('/authors',authorRouter);
